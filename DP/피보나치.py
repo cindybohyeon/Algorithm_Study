6992b23a51a972bb1629b1c552c8075a643db406
bohyeon <980926a@gmail.com>
2022-02-09 11:46:22 +0900

    DP 피보나치 수정

--- DP/피보나치.py
+++ DP/피보나치.py
@@ -1,9 +1,29 @@
+#  n번째 원소 구하기 DP 구현 
+
+
+# 같은 크기에 대해서 같은 계산을 하지 않도록 기억
+# memoization .
+
+# n = 6;
+# memo[7];
+
+# def InitializeMemo():
+#     for i in range(1,n+1):
+#         memo[i] = -1;
+
+
+# 피보나치 수. 
+
 n = int(input())
 
-def fib(n):
-    if n == 1 or n == 2:
+def fibo(n):
+    if n == 1:
         return 1
-    
-    return fib(n - 1) + fib(n - 2)
+    if n == 2:
+        return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
 
-print(fib(n))
+
+print(fibo(n))
+
