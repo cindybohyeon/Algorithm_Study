5c0b638c8848fae85917bdd05ad6aca28f3c2897
bohyeon <980926a@gmail.com>
2022-02-18 20:51:17 +0900

    DFS : index 잘 생각하자

---
+++ DFS, BFS/두방향탈출여부.py
@@ -0,0 +1,108 @@
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0 for _ in range(m)] for _ in range(n)]
+exist = 0
+
+def in_range(x,y):
+    return x>=0 and y>=0 and x< n and y< m
+
+def can_go(nx,ny,visited):
+    return (in_range(nx,ny) and visited[nx][ny] == 0)
+
+
+
+def dfs(x,y):
+    visited[x][y] = 1
+    dx = [1,0]
+    dy = [0,1]
+    for nx,ny in zip(dx,dy):
+        new_x = nx + x
+        new_y = ny + y
+        if(can_go(new_x,new_y,visited)):#범위에 들어가고 방문 안한 것들 
+            if(graph[new_x][new_y]):#뱀이 없는 것
+                visited[new_x][new_y] = 1
+                dfs(new_x,new_y)
+
+dfs(0,0)
+
+
+
+print(visited[n-1][m-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n, m = map(int, input().split())
+
+# graph = [list(map(int, input().split())) for _ in range(n)]
+# visited = [[0 for _ in range(m+1)] for _ in range(n)]
+
+# # 아래, 오른쪽 이동
+# dx = [1,0]
+# dy = [0,1]
+
+# count = 0
+
+# # 함수 이용
+# def in_range(nx,ny):
+#     return nx >= 0 or ny >= 0 or nx < n or ny < m
+# # def in_range(x, y):
+# #     return 0 <= x and x < n and 0 <= y and y < m
+
+# def can_go(x,y):
+#     return in_range(x,y) and graph[x][y] == 1 and visited[x][y] == 0
+
+# def dfs(x,y):
+#     for i in range(2):
+#         new_x = dx[i] + x
+#         new_y = dy[i] + y
+#         if can_go(new_x, new_y):
+#             visited[new_x][new_y] = 1
+#             dfs(new_x, new_y)
+
+     
+# visited[0][0] = 1
+# dfs(0,0)
+# print(visited[n-1][m-1])
+
+# visited = [False for _ in range(N+1)]
+# def dfs(vertex, graph):
+#     '''
+#     :return : vertex 에서 갈 수 있는 모든 정점을 간다
+#     '''
+#     visited[vertex] = True
+#     for adj in (vertex랑 연결된 모든 정점):
+#         if not visited[adj]
+#         # adj에서 다시 갈 수 있는 모든 정점을 확인
+#         dfs(adj, graph)
