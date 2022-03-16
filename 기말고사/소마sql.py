955b507066cb4ea1278ba110da0b1242a24fc60a
bohyeon <980926a@gmail.com>
2022-03-16 11:53:39 +0900

    소마 SQL insert into table values ()

---
+++ 기말고사/소마sql.py
@@ -0,0 +1,5 @@
+insert into customer values (1,'Elice', '2010-01-15', '100');
+update 
+insert into customer values (2,'Cheshire', '2005-03-10', '100');
+insert into customer values (3,'Dodo', '2010-04-30', '100');
+select * from customer
