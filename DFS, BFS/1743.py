94cb9062911565d9593e0e2068d07512495ea683
bohyeon <980926a@gmail.com>
2022-03-18 22:32:35 +0900

    실버 1 BFS 문제

--- DFS, BFS/1743.py
+++ DFS, BFS/1743.py
@@ -1,57 +1,98 @@
-from collections import deque
+# from collections import deque
 
-n,m,k = map(int, input().split())
+# n,m,k = map(int, input().split())
 
-graph = [[0]* (m) for _ in range(n)]
+# graph = [[0]* (m) for _ in range(n)]
 
-for _ in range(k):
-    a,b = map(int,input().split())
-    graph[a-1][b-1] = 1
-# 3 4 5
-# \\\
-# 2 2
-# 3 1
-# 2 3
-# 1 1
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-nx=0
-ny=0
+# for _ in range(k):
+#     a,b = map(int,input().split())
+#     graph[a-1][b-1] = 1
+# # 3 4 5
+# # \\\
+# # 2 2
+# # 3 1
+# # 2 3
+# # 1 1
+# dx = [-1,1,0,0]
+# dy = [0,0,-1,1]
+# nx=0
+# ny=0
 
-# print(graph)
-def bfs(x,y,graph):
-    queue = deque()
-    queue.append((x,y))
-    global count
-    while queue:
-        x,y = queue.popleft()
-        for i in range(4):
-            # 해당 좌표 상하좌우 살피기
-            nx = x + dx[i]
-            ny = y + dy[i]
-            # 상하좌우 위치 아닌것 건너뛰기
-            if(nx<0 or ny<0 or nx>= n or ny>=m):
-                continue
-            # 상하좌우 해당위치가 graph에 있는지 즉 똥이 있는지 확인하기
-            if(graph[nx][ny] == 1):
-                    count += 1
-                    queue.append((nx,ny))
-                    graph[nx][ny] = 0
-
-
-result = 0
-count = 0
-for i in range(n):
-    for j in range(m):
-        if(graph[i][j] == 1):
-            bfs(i,j,graph)
-            result = max(result,count)
-            count = 0
+# # print(graph)
+# def bfs(x,y,graph):
+#     queue = deque()
+#     queue.append((x,y))
+#     global count
+#     while queue:
+#         x,y = queue.popleft()
+#         for i in range(4):
+#             # 해당 좌표 상하좌우 살피기
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             # 상하좌우 위치 아닌것 건너뛰기
+#             if(nx<0 or ny<0 or nx>= n or ny>=m):
+#                 continue
+#             # 상하좌우 해당위치가 graph에 있는지 즉 똥이 있는지 확인하기
+#             if(graph[nx][ny] == 1):
+#                     count += 1
+#                     queue.append((nx,ny))
+#                     graph[nx][ny] = 0
 
 
-print(result)
+# result = 0
+# count = 0
+# for i in range(n):
+#     for j in range(m):
+#         if(graph[i][j] == 1):
+#             bfs(i,j,graph)
+#             result = max(result,count)
+#             count = 0
+
+
+# print(result)
 
 
 # 전역변수 count 사용
 # max 함수 사용
 # 시작 위치 바꾸면서 bfs 진행하기 
+
+
+from collections import deque
+n, m, k = map(int, input().split())
+graph = [[0] * (m+1) for _ in range(n+1)]
+for _ in range(k):
+    a,b = map(int, input().split())
+    graph[a][b] = 1
+
+
+def in_range(x,y):
+    if x > 0 and y > 0 and x <= n and y <= m:
+        return True
+    else:
+        return False
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+    count = 0
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if in_range(nx,ny) and int(graph[nx][ny]):
+                graph[nx][ny] = 0
+                count += 1
+                queue.append((nx,ny))
+    return count
+max_count = 0
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        if int(graph[i][j]):
+            bfs_ = bfs(i,j)
+            max_count = max(bfs_,max_count)
+print(max_count)
+# print(graph)
+# print(bfs(2,2))
--- DFS, BFS/2178.py
+++ DFS, BFS/2178.py
@@ -1,5 +1,5 @@
-from collections import deque
-from types import GeneratorType
+# from collections import deque
+# from types import GeneratorType
 
 # 최단경로 문제 bfs로 푸는지를 몰랐다 ㅠㅠㅠㅠㅠㅠ
 
@@ -11,35 +11,74 @@ from types import GeneratorType
 
 
 # N, M을 공백을 기준으로 구분하여 입력 받기
+# n, m = map(int, input().split())
+# # 2차원 리스트의 맵 정보 입력 받기
+# graph = []
+# for i in range(n):
+#     graph.append(list(map(int, input())))
+# def bfs(x,y,graph):
+#     queue = deque()
+#     queue.append((x,y))
+#     dx = [-1,1,0,0]
+#     dy = [0,0,-1,1]
+#     count=0
+#     while queue:
+#         x,y = queue.popleft()
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             if(nx<0 or ny<0 or nx>= n or ny>=m):
+#                 continue
+#             if(graph[nx][ny] == 0):
+#                 continue
+#             if(graph[nx][ny] == 1):
+#                 # count += 1
+#                 # graph[nx][ny] = count + 1
+#                 queue.append((nx,ny))
+#                 graph[nx][ny] = graph[x][y] + 1
+                
+#     return graph[n-1][m-1]
+
+
+# a = bfs(0,0,graph)
+# print(a)
+
+from collections import deque
 n, m = map(int, input().split())
-# 2차원 리스트의 맵 정보 입력 받기
+# graph = [list(input()) for _ in range(n)]
 graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
-def bfs(x,y,graph):
-    queue = deque()
-    queue.append((x,y))
+
+
+def in_range(x,y):
+    if x >=0 and y >=0 and x < n and y < m:
+        return True
+    else:
+        return False
+
+count = 0
+
+def bfs(x,y):
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
-    count=0
+    global count
+    queue = deque()
+    queue.append((x,y))
+    # graph[x][y] = 0
     while queue:
         x,y = queue.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if(nx<0 or ny<0 or nx>= n or ny>=m):
-                continue
-            if(graph[nx][ny] == 0):
-                continue
-            if(graph[nx][ny] == 1):
-                # count += 1
-                # graph[nx][ny] = count + 1
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if in_range(nx,ny) and int(graph[nx][ny]) == 1:#방문하지 않고 범위 안에 있으면
+                # if nx == n-1 and ny == m-1:
+                #     count += 1
+                #     return
                 queue.append((nx,ny))
-                graph[nx][ny] = graph[x][y] + 1
-                
-    return graph[n-1][m-1]
+                graph[nx][ny] = int(graph[x][y]) + 1
+                count += 1
 
-
-a = bfs(0,0,graph)
-print(a)
-
+bfs(0,0)
+# print(count)
+print(graph[n-1][m-1])
