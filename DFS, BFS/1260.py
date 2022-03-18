9183ee8c4d77fa9e2a4ac0a87720292c1ee674de
bohyeon <980926a@gmail.com>
2022-03-18 19:57:22 +0900

    dfs, bfs 문제 다시 풀어보기

--- DFS, BFS/1260.py
+++ DFS, BFS/1260.py
@@ -1,41 +1,80 @@
+# from collections import deque
+
+
+# # 정점의 개수, 간선의 개수, 탐색 시작할 정점의 번호
+# # 간선이 연결하는 두 정점의 번호
+
+# # map 함수로 입력 받기
+# # 보통 여러 개의 데이터를 한번에 다른 형태로 바꾸기 위해 사용
+# a, b, c = map(int, input().split())
+# graph = [[0]* (a+1) for _ in range(a+1)]
+
+# for _ in range(b):
+#     m1, m2 = map(int, input().split())
+#     graph[m1][m2] = graph[m2][m1] = 1
+
+# visited = [False] * (a+1)
+
+# def dfs(graph, v, visited):
+#     visited[v] = True
+#     print(v, end=' ')
+#     for i in range(1, a+1):
+#         if not visited[i] and graph[v][i]:
+#             dfs(graph, i, visited)
+
+# def bfs(graph, v, visited):
+#     queue = deque([v])
+#     visited[v] = True
+    
+#     while queue:
+#         v = queue.popleft()
+#         print(v, end=' ')
+#         for i in range(1, a+1):
+#             if not visited[i] and graph[v][i]:
+#                 queue.append(i)
+#                 visited[i] = True
+
+
+# dfs(graph, c, visited)
+# visited = [False] * (a+1)
+# print()
+# bfs(graph, c, visited)
+
+
+
 from collections import deque
 
+n,m,v = map(int, input().split())
+# 정점의 개수 간선의 개수 시작 정점 번호
 
-# 정점의 개수, 간선의 개수, 탐색 시작할 정점의 번호
-# 간선이 연결하는 두 정점의 번호
+graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
+visited = [False] * (n+1)
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
 
-# map 함수로 입력 받기
-# 보통 여러 개의 데이터를 한번에 다른 형태로 바꾸기 위해 사용
-a, b, c = map(int, input().split())
-graph = [[0]* (a+1) for _ in range(a+1)]
-
-for _ in range(b):
-    m1, m2 = map(int, input().split())
-    graph[m1][m2] = graph[m2][m1] = 1
-
-visited = [False] * (a+1)
-
-def dfs(graph, v, visited):
+def bfs(graph, visited, v):
+    queue = deque()
+    queue.append(v)s
     visited[v] = True
-    print(v, end=' ')
-    for i in range(1, a+1):
-        if not visited[i] and graph[v][i]:
-            dfs(graph, i, visited)
-
-def bfs(graph, v, visited):
-    queue = deque([v])
-    visited[v] = True
-    
     while queue:
-        v = queue.popleft()
-        print(v, end=' ')
-        for i in range(1, a+1):
-            if not visited[i] and graph[v][i]:
+        x = queue.popleft()
+        print(x, end=' ')
+        for i in range(1,n+1):
+            if graph[x][i] and not visited[i]:
                 queue.append(i)
                 visited[i] = True
+        
+def dfs(graph, visited, v):
+    visited[v] = True
+    print(v, end=' ')
+    for i in range(1,n+1):
+        if graph[v][i] and not visited[i]:
+            dfs(graph, visited, i)
 
-
-dfs(graph, c, visited)
-visited = [False] * (a+1)
+dfs(graph, visited, v)
+# graph = [[0 for _ in range(m+1)] for _ in range(m+1)]
+visited = [False] * (n+1)
 print()
-bfs(graph, c, visited)
+bfs(graph, visited, v)
--- DFS, BFS/1303_2.py
+++ DFS, BFS/1303_2.py
@@ -1,45 +1,97 @@
+# from collections import deque
+
+
+# n, m = map(int, input().split())
+# # graph = [list(input()) for _ in range(m)]
+# graph = [list(input().strip()) for _ in range(n)]
+
+# dx = [-1,1,0,0]
+# dy = [0,0,-1,1]
+
+# def bfs(x,y,team):
+#     queue = deque()
+#     queue.append((x,y))
+#     count = 0
+#     nx = 0
+#     ny = 0
+#     while queue:
+#         x,y=queue.popleft()
+#         for i in range(4):
+#             nx = dx[i] + x
+#             ny = dy[i] + y
+#             if nx<0 or ny<0 or nx >= n or ny >= n:
+#                 continue # 아래의 if 넘어가서 for문 다음 i 로 이동
+#             if graph[nx][ny] != 0 and graph[nx][ny] == team:
+#                 queue.append((nx,ny))
+#                 graph[nx][ny] = 0
+#                 count += 1
+#     return (1 if count == 0 else count)
+
+
+
+# w = 0
+# b = 0
+
+
+# # 방문 표시를 하기 때문에 
+# for i in range(n):
+#     for j in range(m):
+#         if graph[i][j] != 0:
+#             if graph[i][j] == "W":
+#                 w += bfs(i,j,graph[i][j])**2
+#             if graph[i][j] == 'B':
+#                 b += bfs(i,j,graph[i][j])**2
+
+# print(w,b)
+
 from collections import deque
+n,m = map(int, input().split())
+# 이중 배열 입력 받기
+graph = [list(input()) for _ in range(m)]
+visited = [[0] * (n) for _ in range(m)]
+
+def in_range(x,y):
+    if x >= 0 and y >= 0 and x < n and y < n:
+        return True
+    else:
+        return False
 
 
-n, m = map(int, input().split())
-# graph = [list(input()) for _ in range(m)]
-graph = [list(input().strip()) for _ in range(n)]
-
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-
-def bfs(x,y,team):
+def bfs(x,y, team):
+    count = 1
     queue = deque()
     queue.append((x,y))
-    count = 0
-    nx = 0
-    ny = 0
+    visited[x][y] = 1
     while queue:
-        x,y=queue.popleft()
+        x,y = queue.popleft()
+        dx = [-1,1,0,0]
+        dy = [0,0,-1,1]
         for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if nx<0 or ny<0 or nx >= n or ny >= n:
-                continue # 아래의 if 넘어가서 for문 다음 i 로 이동
-            if graph[nx][ny] != 0 and graph[nx][ny] == team:
-                queue.append((nx,ny))
-                graph[nx][ny] = 0
-                count += 1
-    return (1 if count == 0 else count)
-
+            nx = x + dx[i]
+            ny = y + dy[i]     
+            if in_range(nx,ny) and not visited[nx][ny]:
+                if graph[nx][ny] == team:
+                    count += 1
+                    queue.append((nx,ny))
+                    visited[nx][ny] = 1
+    if count == 0 : return 1
+    else: return count
 
 
 w = 0
 b = 0
 
-
 # 방문 표시를 하기 때문에 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] != 0:
+for i in range(m):
+    for j in range(n):
+        if not visited[i][j]:
             if graph[i][j] == "W":
                 w += bfs(i,j,graph[i][j])**2
             if graph[i][j] == 'B':
                 b += bfs(i,j,graph[i][j])**2
 
+# bfs(0,0,graph[0][0])
+# print(bfs(0,0,graph[0][0]))
 print(w,b)
+
+
