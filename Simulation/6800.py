c39683acb9ae0c2fd808d63314755d06043e46bc
bohyeon <980926a@gmail.com>
2022-05-08 23:54:22 +0900

    ict 대비 공부한 애나그램

---
+++ Simulation/6800.py
@@ -0,0 +1,19 @@
+import string
+
+
+n = int(input())
+charactor = []
+number = []
+for _ in range(n):
+    a, b = input().split()
+
+    charactor.append(a)
+    number.append(int(b))
+
+ans = []
+ans.append(int(input()))
+
+# print(charactor)
+# print(number)
+
+# 
