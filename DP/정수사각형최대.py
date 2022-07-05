ebb604d05a0acfefe31ccca26de80baccd25b1e8
bohyeon <980926a@gmail.com>
2022-07-05 20:45:29 +0900

    DP

--- DP/정수사각형최대.py
+++ DP/정수사각형최대.py
@@ -1,28 +1,51 @@
 
+# n = int(input())
+# graph = [list(map(int, input().split())) for _ in range(n)]
+# dp = [[0 for _ in range(n)] for _ in range(n)]
+
+# dp[0][0] = graph[0][0]
+# def initialize():
+#     # 시작점의 경우 dp[0][0] = num[0][0]으로 초기값을 설정해줍니다
+#     dp[0][0] = graph[0][0]
+    
+#     # 최좌측 열의 초기값을 설정해줍니다.
+#     for i in range(1, n):
+#         dp[i][0] = dp[i-1][0] + graph[i][0]
+    
+#     # 최상단 행의 초기값을 설정해줍니다.
+#     for j in range(1, n):
+#         dp[0][j] = dp[0][j-1] + graph[0][j]
+        
+        
+# # 초기값 설정
+# initialize()
+
+# for i in range(1,n):
+#     for j in range(1,n):
+#         dp[i][j] = max(dp[i][j-1], dp[i-1][j]) + graph[i][j]
+
+
+# print(dp[n-1][n-1])
+
+
+
 n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
-dp = [[0 for _ in range(n)] for _ in range(n)]
+graph = [ list(map(int, input().split())) for _ in range(n)]
+dp = [ [0 for _ in range(n)] for _ in range(n)]
 
-dp[0][0] = graph[0][0]
-def initialize():
-    # 시작점의 경우 dp[0][0] = num[0][0]으로 초기값을 설정해줍니다
+# 오른쪽 이나 밑으로 이동한다.
+
+def initialize(dp):
+    
     dp[0][0] = graph[0][0]
-    
-    # 최좌측 열의 초기값을 설정해줍니다.
-    for i in range(1, n):
+    for i in range(1,n):
         dp[i][0] = dp[i-1][0] + graph[i][0]
-    
-    # 최상단 행의 초기값을 설정해줍니다.
-    for j in range(1, n):
-        dp[0][j] = dp[0][j-1] + graph[0][j]
-        
-        
-# 초기값 설정
-initialize()
+        dp[0][i] = dp[0][i-1] + graph[0][i]
 
-for i in range(1,n):
+initialize(dp)
+
+for i in range(1, n):
     for j in range(1,n):
-        dp[i][j] = max(dp[i][j-1], dp[i-1][j]) + graph[i][j]
-
+        dp[i][j] = max((dp[i-1][j] + graph[i][j]) , (dp[i][j-1] + graph[i][j]))
 
 print(dp[n-1][n-1])
--- DP/피보나치.py
+++ DP/피보나치.py
@@ -1,29 +1,40 @@
-#  n번째 원소 구하기 DP 구현 
+# #  n번째 원소 구하기 DP 구현 
 
 
-# 같은 크기에 대해서 같은 계산을 하지 않도록 기억
-# memoization .
+# # 같은 크기에 대해서 같은 계산을 하지 않도록 기억
+# # memoization .
 
-# n = 6;
-# memo[7];
+# # n = 6;
+# # memo[7];
 
-# def InitializeMemo():
-#     for i in range(1,n+1):
-#         memo[i] = -1;
-
-
-# 피보나치 수. 
-
-n = int(input())
-
-def fibo(n):
-    if n == 1:
-        return 1
-    if n == 2:
-        return 1
-    else:
-        return fibo(n-1) + fibo(n-2)
+# # def InitializeMemo():
+# #     for i in range(1,n+1):
+# #         memo[i] = -1;
 
 
-print(fibo(n))
+# # 피보나치 수. 
 
+# n = int(input())
+
+# def fibo(n):
+#     if n == 1:
+#         return 1
+#     if n == 2:
+#         return 1
+#     else:
+#         return fibo(n-1) + fibo(n-2)
+
+
+# print(fibo(n))
+
+
+n = 4
+dp = [ 0 for _ in range(n+1)]
+
+dp[1] = 1
+dp[2] = 1
+
+for i in range(3,n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[4])
