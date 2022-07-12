7a7fccd4e4a4aab2790b5547fd9d463e6fa7bfd9
bohyeon <980926a@gmail.com>
2022-07-12 10:09:47 +0900

    복습

--- Greedy/1이될때까지.py
+++ Greedy/1이될때까지.py
@@ -1,33 +1,44 @@
-# 어떠한 수 N이 1이 될 때까지 두 과정 중 하나를 반복적으로 선택.
-# 두번째는 N이 K로 떨어질 때만 선택가능.
+# # 어떠한 수 N이 1이 될 때까지 두 과정 중 하나를 반복적으로 선택.
+# # 두번째는 N이 K로 떨어질 때만 선택가능.
 
-# n에서 1을 뺀다. / N을 K로 나눈다.
+# # n에서 1을 뺀다. / N을 K로 나눈다.
 
-n,k = map(int, input().split())
-result = 0
+# n,k = map(int, input().split())
+# result = 0
+# # while True:
+# #     if n == 1:
+# #         break
+# #     if n%k == 0:
+# #         n = n/k
+# #         result += 1
+# #     else:
+# #         n -= 1
+# #         result += 1
+
 # while True:
-#     if n == 1:
+#     # N==K 로 나누어 떨어지는 수 가 될 때까지 1씩 빼기
+#     target = (n/k)* k
+#     result += (n-target)
+#     n = target
+#     # n이 k 보다 작을 때 즉 더이상 나눌 수 없을 때까지
+#     if n<k:
 #         break
-#     if n%k == 0:
-#         n = n/k
-#         result += 1
-#     else:
-#         n -= 1
-#         result += 1
+#     # k로 나누기
+#     result += 1
+#     n = n/k
 
-while True:
-    # N==K 로 나누어 떨어지는 수 가 될 때까지 1씩 빼기
-    target = (n/k)* k
-    result += (n-target)
-    n = target
-    # n이 k 보다 작을 때 즉 더이상 나눌 수 없을 때까지
-    if n<k:
-        break
-    # k로 나누기
-    result += 1
-    n = n/k
+# # 마지막으로 남은 수에 대하여 1씩 빼기 : n-1한 이유는 n==1이 될 때까지 빼는거니까.
+# result += (n-1)
 
-# 마지막으로 남은 수에 대하여 1씩 빼기 : n-1한 이유는 n==1이 될 때까지 빼는거니까.
-result += (n-1)
+# print(result)
 
-print(result)
+n, k = map(int, input().split())
+result = 0
+while n>=k:#n을 k로 나누니가. 
+    if n % k != 0:
+        n -= 1
+        result += 1
+    else:
+        n = n/k
+        result += 1
+    
--- Greedy/큰수의법칙.py
+++ Greedy/큰수의법칙.py
@@ -1,31 +1,46 @@
-# 주어진 수들을 M 번 더하여 가장 큰 수를 만드는 법칙이다. 
-# 하지만 하나의 배열의 원소가 연속해서 K번을 초과하여 더해질 수 없다.
+# # 주어진 수들을 M 번 더하여 가장 큰 수를 만드는 법칙이다. 
+# # 하지만 하나의 배열의 원소가 연속해서 K번을 초과하여 더해질 수 없다.
 
-# 문제 풀이 : 제일 큰 값 K 번 + 그 다음 큰 값 1번 + 제일 큰 값 K 번 + 그 다음 큰 값 1번 ...
+# # 문제 풀이 : 제일 큰 값 K 번 + 그 다음 큰 값 1번 + 제일 큰 값 K 번 + 그 다음 큰 값 1번 ...
 
-# n,m,k 공백구분하여 한줄로 입력받기 
+# # n,m,k 공백구분하여 한줄로 입력받기 
+# n,m,k = map(int, input().split())
+# # 
+# array = list(map(int, input().split()))
+
+# # array  정렬하기. 가장 큰 수를 먼저 선택할 수 있도록
+# array.sort()
+# first = array[n-1]
+# second = array[n-2]
+
+# ans = 0
+
+# while True:
+
+#     for _ in range(k):
+#         if m == 0:
+#             break
+#         ans += first
+#         m -= 1
+#     if m == 0:
+#         break
+    
+#     ans += second
+#     m -= 1
+
+# print(ans)
+    
+
 n,m,k = map(int, input().split())
-# 
-array = list(map(int, input().split()))
+# 배열의 크기, 숫자가 더해지는 횟수, 연속으로 더해질 수 있는 횟수
+num = list(map(int, input().split()))
 
-# array  정렬하기. 가장 큰 수를 먼저 선택할 수 있도록
-array.sort()
-first = array[n-1]
-second = array[n-2]
-
+# 제일 큰 수를 k 번 더하고 다음 큰 수 한번 더하고 ---
+# 수열의  길이 k+1
+list_len = m/(k+1) + m%(k+1)
+num.sort()
+max_len = m/(k+1) * k + m%(k+1)
+next_len = m - max_len
 ans = 0
-
-while True:
-    for _ in range(k):
-        if m == 0:
-            break
-        ans += first
-        m -= 1
-    if m == 0:
-        break
-    
-    ans += second
-    m -= 1
-
+ans = num[n-1] * max_len + num[n-2] * next_len
 print(ans)
-    
