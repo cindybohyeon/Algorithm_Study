da8c7b8f2d8d1c29484556e3bf7c83da59973acc
cindybohyeon <bohyeoncindy@gmail.com>
2024-02-21 21:30:10 +0900

    feat: dp 문제

--- 2024/11660.py
+++ 2024/11660.py
@@ -9,6 +9,20 @@ test = []
 for _ in range(m):
     test.append(list(map(int, input().split())))
 
+dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1] + graph[i - 1][j - 1] - dp[i-1][j-1]
+
+for x1, y1, x2, y2 in test:
+    ans = 0
+
+    ans = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1]
+
+    print(ans)
+
 
 # def findSum(x1, y1, x2, y2):
 #     global graph
@@ -34,34 +48,6 @@ for _ in range(m):
 #     # print(dp)
 #     return dp[xLen - 1][yLen - 1]
     
-dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
-# dp[0][0] = graph[0][0]
-# for i in range(1, n):
-#     dp[0][i] = graph[0][i] + dp[0][i-1]
-#     dp[i][0] = graph[i][0] + dp[i-1][0]
-
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        dp[i][j] = dp[i-1][j] + dp[i][j-1] + graph[i - 1][j - 1] - dp[i-1][j-1]
-
-print(dp, "dp")
-# dp2 = [[0]*(n+1) for i in range(n+1)]
-
-# for i in range(1, n+1):
-#     for j in range(1, n+1):
-#         dp2[i][j] = dp2[i][j-1] + dp2[i-1][j] - dp2[i-1][j-1] + graph[i-1][j-1]
-
-# print(dp2,"dp2")
-for x1, y1, x2, y2 in test:
-    ans = 0
-
-    ans = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1]
-    # print("a", ans)
-    # for i in range(0, y1-1):
-    #     ans -= dp[x2-1][i]
-    # for i in range(0, x1-1):
-    #     ans -= dp[i][y2-1]
-    print(ans)
 
     
 
