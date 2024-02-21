673b305d561811a83500097f38b2449da4bd9c08
cindybohyeon <bohyeoncindy@gmail.com>
2024-02-21 21:26:27 +0900

    feat: 기초체력훈련 다시풀기

--- 2024/기초체력훈련21/1940.py
+++ 2024/기초체력훈련21/1940.py
@@ -7,9 +7,10 @@ graph.sort()
 start = 0
 end = n - 1
 
-while(start <= end):
+while(start < end):
     temp = graph[start] + graph[end]
     if temp == m:
+        # print( graph[start], graph[end])
         result += 1
         start += 1
         end -= 1
--- 2024/기초체력훈련21/2018.py
+++ 2024/기초체력훈련21/2018.py
@@ -1,7 +1,27 @@
 n = int(input())
 # 연속된 자연수로.
-result = 0
+result = 1
 count = n // 2 + n % 2
+
+start = 1
+end = 2
+temp = start + end
+while(start <= count and end <= count):    
+    if temp == n:
+        # print(start, end)
+        temp -= start
+        start += 1
+        result += 1
+    
+    elif temp < n:
+        end += 1
+        if end <= count:
+            temp += end
+    else:
+        temp -= start
+        start += 1
+        
+print(result)        
 
 # temp = 0
 # for i in range(count, 1, -1):
@@ -9,33 +29,33 @@ count = n // 2 + n % 2
 #     if temp == n:
 #         result += 1
 
-dp = [0 for _ in range(n + 1)]
-for i in range(1, n):
-    dp[i] = dp[i - 1] + i
+# dp = [0 for _ in range(n + 1)]
+# for i in range(1, n):
+#     dp[i] = dp[i - 1] + i
 
-# print(dp)
+# # print(dp)
 
-for i in range(count, 0, -1):
-    for j in range(count):
-        temp = dp[i] - dp[j] + j
-        if temp == n:
-            result += 1
+# for i in range(count, 0, -1):
+#     for j in range(count):
+#         temp = dp[i] - dp[j] + j
+#         if temp == n:
+#             result += 1
 
 
-start = 0
-end = count
+# start = 0
+# end = count
 
-while(start <= end):
-    temp = dp[end] - dp[start] + start
-    if temp == n:
-        result += 1
-        print(start, end, "@@")
-        start += 1
-        end -= 1
+# while(start <= end):
+#     temp = dp[end] - dp[start] + start
+#     if temp == n:
+#         result += 1
+#         print(start, end, "@@")
+#         start += 1
+#         end -= 1
 
-    elif temp > n:
-        end -= 1
-    else:
-        start += 1
+#     elif temp > n:
+#         end -= 1
+#     else:
+#         start += 1
 
-print(result)
+
